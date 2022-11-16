6f72c37321ea220d659de0dcedd96e5336ceea63
Elson Costa <elsoncosta@MacBook-Air-de-Elson.local>
2022-11-16 17:54:40 -0300

    add collum datetime

--- TesteJonB/Models/SomeEntity.cs
+++ TesteJonB/Models/SomeEntity.cs
@@ -3,6 +3,7 @@ namespace TesteJonB.Models
     public class SomeEntity
     {
         public int Id { get; set; }
+        public DateTime DataCadastro { get; set; }
         public Customer Customer { get; set; }
     }
 }
